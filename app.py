2f645f60548769f516f6fad6a41d3fc8c6fc1c09
Avrami Hammer <avramihammer@gmail.com>
2018-09-11 16:40:26 +1000

    added created_at stuff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import time
 import traceback
 from functools import wraps
 
@@ -35,18 +36,61 @@ def requires_auth(f):
     return decorated
 
 
+def db_wrapper(f):
+    @requires_auth
+    def inner(*args, **kwargs):
+        host = os.getenv("ENDPOINT")
+        user = os.getenv("USER")
+        password = os.getenv("PASSWORD")
+        db = os.getenv("DATABASE")
+
+        connection = pymysql.connect(host=host, user=user, password=password, db=db,
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        return f(connection=connection, *args, **kwargs)
+
+    return inner
+
+
+def db_get_wrapper(f):
+    func_args = f.__code__.co_varnames
+
+    @db_wrapper
+    def inner(*args, **kwargs):
+        since = request.args.get("since", 0, type=int)
+        before = request.args.get("before", time.time(), type=int)
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 10, type=int)
+        keys = {"since": since, "before": before, "page": page, "per_page": per_page}
+        for key in keys:
+            if key not in func_args:
+                keys.pop(key)
+        return f(*args, **keys, **kwargs)
+
+    return inner
+
+
+def database(f=None, request_type=None):
+    if f is not None:
+        return db_wrapper(f)
+    if request_type == "GET":
+        return db_get_wrapper
+    else:
+        return db_wrapper
+
+
 @app.route('/')
 def index():
     return Response('Online', status=200)
 
 
 @app.route('/users', methods=['GET'])
-@requires_auth
-def get_users():
+@database(request_type="GET")
+def get_users(connection, page, per_page):
     try:
-        with connect().cursor() as cursor:
-            query = """SELECT `author_id` as USER FROM Users"""
-            cursor.execute(query)
+        with connection.cursor() as cursor:
+            query = """SELECT `author_id` as USER FROM Users LIMIT %s, %s"""
+            cursor.execute(query, [per_page, page])
             result = cursor.fetchall()
             return jsonify(result)
     except:
@@ -55,12 +99,12 @@ def get_users():
 
 
 @app.route('/channels', methods=['GET'])
-@requires_auth
-def get_channels():
+@database(request_type="GET")
+def get_channels(connection, page, per_page):
     try:
-        with connect().cursor() as cursor:
-            query = """SELECT channel_id, `COUNT(id)` as messages FROM Channels"""
-            cursor.execute(query)
+        with connection.cursor() as cursor:
+            query = """SELECT channel_id, `COUNT(id)` as messages FROM Channels LIMIT %s, %s"""
+            cursor.execute(query, [per_page, page])
             result = cursor.fetchall()
             return jsonify(result)
     except:
@@ -70,11 +114,16 @@ def get_channels():
 
 @app.route('/users/<user_id>/messages', methods=['GET'])
 @requires_auth
-def get_message_by_user(user_id):
+@database(request_type='GET')
+def get_message_by_user(connection, since, before, page, per_page, user_id):
     try:
-        with connect().cursor() as cursor:
-            query = """SELECT * FROM MESSAGES WHERE `author_id` = %s"""
-            cursor.execute(query, user_id)
+        with connection.cursor() as cursor:
+            query = """SELECT id, channel_id, author_id, content FROM `MESSAGES` WHERE `author_id` = %s
+            AND `Created_At` > from_unixtime(%s)
+            AND `Created_At` < from_unixtime(%s)
+            LIMIT %s, %s
+            """
+            cursor.execute(query, [user_id, since, before, per_page, page])
             result = cursor.fetchall()
             return jsonify(result)
     except:
@@ -84,11 +133,15 @@ def get_message_by_user(user_id):
 
 @app.route('/channels/<channel_id>/messages', methods=['GET'])
 @requires_auth
-def get_messages(channel_id):
+@database(request_type='GET')
+def get_messages(connection, since, before, per_page, page, channel_id):
     try:
-        with connect().cursor() as cursor:
-            query = """SELECT * FROM MESSAGES WHERE `channel_id` = %s"""
-            cursor.execute(query, channel_id)
+        with connection.cursor() as cursor:
+            query = """SELECT id, channel_id, author_id, content FROM `MESSAGES` WHERE `channel_id` = %s
+            AND `Created_At` > from_unixtime(%s)
+            AND `Created_At` < from_unixtime(%s)
+            LIMIT %s, %s"""
+            cursor.execute(query, [channel_id, since, before, per_page, page])
             result = cursor.fetchall()
             return jsonify(result)
     except:
@@ -98,17 +151,19 @@ def get_messages(channel_id):
 
 @app.route('/channels/<channel_id>/messages', methods=['POST', 'PUT'])
 @requires_auth
-def create_message(channel_id):
+@database
+def create_message(connection, channel_id):
     if "content" not in request.form or "author_id" not in request.form:
         return Response("The request is incomplete/missing values", 403)
 
     content = request.form["content"]
     author = request.form["author_id"]
+
     try:
-        connection = connect()
         with connection.cursor() as cursor:
-            query = """INSERT INTO MESSAGES VALUES(NULL, %s, %s, %s)"""
-            cursor.execute(query, (channel_id, author, content))
+            query = """INSERT INTO MESSAGES (`channel_id`, `author_id`, `content`, `Modified_By`)
+             VALUES(%s, %s, %s, %s)"""
+            cursor.execute(query, (channel_id, author, content, request.remote_addr))
             connection.commit()
             return jsonify({
                 "result": "success",
@@ -117,22 +172,6 @@ def create_message(channel_id):
     except:
         traceback.print_exc()
         return Response("A Database Error Occurred", 500)
-
-
-def connect():
-    host = os.getenv("ENDPOINT")
-    user = os.getenv("USER")
-    password = os.getenv("PASSWORD")
-    # db = os.getenv("DATABASE")
-    db = "messaging"
-    print(host)
-    print(user)
-    print(password)
-    print(db)
-
-    connection = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor)
-
-    return connection
 
 
 if __name__ == '__main__':
