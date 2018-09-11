a4159cec201e0d35b772c502c7b7ba6cd7c877e6
Avrami Hammer <avramihammer@gmail.com>
2018-09-11 17:09:56 +1000

    fixed logic/syntax errors in initial implementation

--- app.py
+++ app.py
@@ -36,7 +36,10 @@ def requires_auth(f):
     return decorated
 
 
-def db_wrapper(f):
+def database(f):
+    func_args = f.__code__.co_varnames
+
+    @wraps(f)
     @requires_auth
     def inner(*args, **kwargs):
         host = os.getenv("ENDPOINT")
@@ -46,37 +49,15 @@ def db_wrapper(f):
 
         connection = pymysql.connect(host=host, user=user, password=password, db=db,
                                      cursorclass=pymysql.cursors.DictCursor)
-
-        return f(connection=connection, *args, **kwargs)
-
-    return inner
-
-
-def db_get_wrapper(f):
-    func_args = f.__code__.co_varnames
-
-    @db_wrapper
-    def inner(*args, **kwargs):
         since = request.args.get("since", 0, type=int)
         before = request.args.get("before", time.time(), type=int)
-        page = request.args.get("page", 1, type=int)
+        page = request.args.get("page", 0, type=int)
         per_page = request.args.get("per_page", 10, type=int)
-        keys = {"since": since, "before": before, "page": page, "per_page": per_page}
-        for key in keys:
-            if key not in func_args:
-                keys.pop(key)
-        return f(*args, **keys, **kwargs)
+        query = {"since": since, "before": before, "page": page, "per_page": per_page}
+        query_args = {k: query[k] for k in func_args if k in query and k not in kwargs}
+        return f(connection=connection, *args, **query_args, **kwargs)
 
     return inner
-
-
-def database(f=None, request_type=None):
-    if f is not None:
-        return db_wrapper(f)
-    if request_type == "GET":
-        return db_get_wrapper
-    else:
-        return db_wrapper
 
 
 @app.route('/')
@@ -85,11 +66,11 @@ def index():
 
 
 @app.route('/users', methods=['GET'])
-@database(request_type="GET")
+@database
 def get_users(connection, page, per_page):
     try:
         with connection.cursor() as cursor:
-            query = """SELECT `author_id` as USER FROM Users LIMIT %s, %s"""
+            query = """SELECT `author_id` as Name FROM Users LIMIT %s OFFSET %s"""
             cursor.execute(query, (per_page, page))
             result = cursor.fetchall()
             return jsonify(result)
@@ -99,11 +80,11 @@ def get_users(connection, page, per_page):
 
 
 @app.route('/channels', methods=['GET'])
-@database(request_type="GET")
+@database
 def get_channels(connection, page, per_page):
     try:
         with connection.cursor() as cursor:
-            query = """SELECT channel_id, `COUNT(id)` as messages FROM Channels LIMIT %s, %s"""
+            query = """SELECT channel_id, `COUNT(id)` as messages FROM Channels LIMIT %s OFFSET %s"""
             cursor.execute(query, (per_page, page))
             result = cursor.fetchall()
             return jsonify(result)
@@ -113,16 +94,16 @@ def get_channels(connection, page, per_page):
 
 
 @app.route('/users/<user_id>/messages', methods=['GET'])
-@requires_auth
-@database(request_type='GET')
+@database
 def get_message_by_user(connection, since, before, page, per_page, user_id):
     try:
         with connection.cursor() as cursor:
             query = """SELECT id, channel_id, author_id, content FROM `MESSAGES` WHERE `author_id` = %s
             AND `Created_At` > from_unixtime(%s)
             AND `Created_At` < from_unixtime(%s)
-            LIMIT %s, %s
+            LIMIT %s OFFSET %s
             """
+            print(query % (user_id, since, before, per_page, page))
             cursor.execute(query, (user_id, since, before, per_page, page))
             result = cursor.fetchall()
             return jsonify(result)
@@ -133,16 +114,18 @@ def get_message_by_user(connection, since, before, page, per_page, user_id):
 
 @app.route('/channels/<channel_id>/messages', methods=['GET'])
 @requires_auth
-@database(request_type='GET')
+@database
 def get_messages(connection, since, before, per_page, page, channel_id):
     try:
         with connection.cursor() as cursor:
             query = """SELECT id, channel_id, author_id, content FROM `MESSAGES` WHERE `channel_id` = %s
-            AND `Created_At` > from_unixtime(%s)
+            AND `Created_At` >= from_unixtime(%s)
             AND `Created_At` < from_unixtime(%s)
-            LIMIT %s, %s"""
+            LIMIT %s OFFSET %s"""
             cursor.execute(query, (channel_id, since, before, per_page, page))
             result = cursor.fetchall()
+            print(query % (channel_id, since, before, per_page, page))
+
             return jsonify(result)
     except:
         traceback.print_exc()
